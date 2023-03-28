894f17430d259441df942f12ab947bf0fe688c48
Miilyb <cberrios195@gmail.com>
2023-03-28 01:55:41 -0300

    avance 3 htmlcss

---
+++ ProyectoIntegrador2/proyectointegradordos/src/main/java/com/proyectointegradordos/proyectointegradordos/ProyectointegradordosApplication.java
@@ -0,0 +1,13 @@
+package com.proyectointegradordos.proyectointegradordos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectointegradordosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectointegradordosApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador2/proyectointegradordos/src/test/java/com/proyectointegradordos/proyectointegradordos/ProyectointegradordosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectointegradordos.proyectointegradordos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectointegradordosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
